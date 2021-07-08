20733f224f34d761948c4d45c17344f180247adb
emusteric <ben.hollamby@outlook.com>
2021-07-08 13:51:04 +1200

    formatting

--- I-S-Challenge 1.ps1
+++ I-S-Challenge 1.ps1
@@ -2,19 +2,24 @@
 # and SET the registered user and organization for the local Windows computer.
 # This is information you can see when you run the winver command. 
 # This information is stored in the Windows registry.
+
 function Get-OrganisationDetails {
-    $owner = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\' `
-    -Name RegisteredOwner).RegisteredOwner
+
+    $owner = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\' -Name RegisteredOwner).RegisteredOwner
     $ownerstring = (Get-culture).TextInfo.ToTitleCase($owner)
 
-    $org = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\' `
-    -Name RegisteredOrganization).RegisteredOrganization
+    $org = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\' -Name RegisteredOrganization).RegisteredOrganization
     $orgstring = (Get-culture).TextInfo.ToTitleCase($org)
 
-    Write-Host -ForegroundColor Green "Owner of this machine is: $ownerstring"
+    Write-Host -ForegroundColor Green "Owner of this machine is: $ownerstring `nOrganisation is: $orgstring"
 
-    Write-Host -ForegroundColor Green "Organisation is: $orgstring"
 }
 
 # For the SET function, you should let the user specify if they want to set the user and/or organization.
 # The output should show the computer name and the registered values.
+
+function Set-OrganisationDetails {
+    
+    
+    
+}
