e7358e52c994070e62b5305ba6fc077e7f8af23e
emusteric <ben.hollamby@outlook.com>
2021-07-14 16:46:56 +1200

    created a couple of scripts today at work

---
+++ Work/Set-Primary-Language-With-Maori-Keyboard.ps1
@@ -0,0 +1,49 @@
+﻿#this script checks if you are in Australia or NZ, and then sets your primary language.
+#if you are in NZ, it also sets you keyboard to use Maori keyboard.
+
+$location = Get-WinHomeLocation | Select-Object -ExpandProperty HomeLocation
+
+if ($location -eq "New Zealand") { 
+
+    Write-Host -ForegroundColor Green "Country is NZ"
+
+    $language = New-WinUserLanguageList -Language "en-NZ"
+    $language.add("en-US")                                      #Copy this line as many times as you like and paste directly below it to add more languages. You can also remove this line altogether to ditch the US keyboard.
+
+    try {
+
+        Set-WinUserLanguageList -LanguageList $language -Force
+        Set-ItemProperty -Path 'HKLM:\SYSTEM\ControlSet001\Control\Keyboard Layouts\00000409\' -Name "Layout File" -Value KBDMAORI.DLL
+
+        }
+        catch {
+
+            Write-Host -ForegroundColor Red "Catastrophic failure of unknown proportions"
+            
+            }        
+
+}elseif ($location -eq "Australia") {
+
+    Write-Host -ForegroundColor Yellow "Country is Australia"
+
+    $language = New-WinUserLanguageList -Language "en-AU"
+    $language.add("en-US")                                      
+
+    try {
+
+        Set-WinUserLanguageList -LanguageList $language -Force
+
+        }
+        catch {
+
+            Write-Host -ForegroundColor Red "Catastrophic failure of unknown proportions"
+
+            }
+
+}else {
+
+    Write-Host -ForegroundColor Red "Country not either NZ or Australia, leaving alone."
+
+} 
+
+
---
+++ Work/Set_Maori_Keyboard.ps1
@@ -0,0 +1,32 @@
+﻿$path = (Get-ItemProperty -Path 'HKLM:\SYSTEM\ControlSet001\Control\Keyboard Layouts\00000409' -Name "Layout File").'Layout File'
+
+if ($path -ne "KBDUS.DLL") { 
+
+        Write-Host -ForegroundColor Yellow "Layout File is $path"
+
+}else {
+
+    try {
+
+        Set-ItemProperty -Path 'HKLM:\SYSTEM\ControlSet001\Control\Keyboard Layouts\00000409\' -Name "Layout File" -Value KBDMAORI.DLL
+        
+    }
+    catch {
+
+        Write-Host -ForegroundColor Red "Failed to change the layout file"
+
+    }
+    
+}
+
+$output = (Get-ItemProperty -Path 'HKLM:\SYSTEM\ControlSet001\Control\Keyboard Layouts\00000409' -Name "Layout File").'Layout File'
+
+if ($output -eq "KBDMAORI.DLL") {
+
+    Write-host -ForegroundColor Green "Value is $output"
+
+    }else {
+
+        Write-Host -ForegroundColor Red "Value is $output"
+
+        }
