79c37658ad9ad5f2c35c26bf8010b647395367fe
emusteric <ben.hollamby@outlook.com>
2021-07-19 20:12:59 +1200

    More advanced function p1

---
+++ Get-ComputerInfo.ps1
@@ -0,0 +1,46 @@
+﻿function Get-SystemInfo {
+
+    [CmdletBinding()]
+
+    param (
+        
+        [string[]]$ComputerName,
+
+        [string]$ErrorLog = "logforerror"
+    )
+
+    BEGIN {
+
+        Write-Output "Log name is $errorlog"
+
+    }
+
+    PROCESS {
+
+        foreach ($computer in $ComputerName) {
+        
+            $os = Get-WmiObject -Class Win32_OperatingSystem -ComputerName $computer
+
+            $comp = Get-WmiObject -Class Win32_ComputerSystem -ComputerName $computer
+
+            $bios = Get-WmiObject -Class Win32_BIOS -ComputerName $computer
+
+            $properties = @{'ComputerName'=$computer;
+                            'OSVersion'=$os.version;
+                            'SPVersion'=$os.servicepackmajorversion;
+                            'BIOSSerial'=$bios.serialnumber;
+                            'Manufacturer'=$comp.manufacturer;
+                            'Model'=$comp.model}
+
+            $object = New-Object -TypeName PSObject -Property $properties
+
+            Write-Output $object
+
+        }
+
+    }
+
+    END {               
+
+    }
+}
