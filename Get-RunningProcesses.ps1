28dda5e261f4a9e613ef2c65cc4b7e4d5ef8deeb
emusteric <ben.hollamby@outlook.com>
2021-07-20 10:59:39 +1200

    More good learnin

---
+++ Get-RunningProcesses.ps1
@@ -0,0 +1,47 @@
+﻿function Get-RunningProcesses {
+
+    [cmdletbinding()]
+
+    param (
+
+        [Parameter(Mandatory = $true)]
+        [string[]]$ComputerName
+
+    )
+
+    BEGIN {
+
+    }
+
+    PROCESS {
+
+        foreach ($computer in $ComputerName) {
+
+            $IDS = Get-WmiObject -Class Win32_Service | Where-Object {$_.State -eq "running"} | Select-Object -ExpandProperty ProcessID
+
+            foreach ($id in $IDS) {
+
+                $Process = Get-WmiObject -Class Win32_Process | Where-Object {$_.ProcessID -eq "$id"}
+
+                $properties = @{'ComputerName'=$computer;
+                                'ThreadCount'=$Process.ThreadCount;
+                                'ProcessName'=$Process.ProcessName;
+                                'Name'=$Process.Name;
+                                'VMSize'=$Process.VM;
+                                'PeakPageFile'=$Process.PeakPageFileUsage
+                                }
+                
+                $object = New-Object -TypeName PSObject -Property $properties
+
+                Write-Output $object
+                
+            }
+        }
+
+    }
+
+    END {
+
+    }
+
+}
---
+++ Get-SystemInfo.ps1
@@ -0,0 +1,42 @@
+﻿function Get-SystemInfo {
+
+    [cmdletbinding()]
+
+    param (
+
+        [Parameter(Mandatory = $true)]
+        [string[]]$ComputerName
+
+    )
+
+    BEGIN {
+
+    }
+
+    PROCESS {
+
+        foreach ($computer in $ComputerName) {
+        
+            $OS = Get-WmiObject -Class Win32_OperatingSystem
+
+            $CS = Get-WmiObject -Class Win32_ComputerSystem
+
+            $properties = @{'Model'=$CS.Model;
+                            'ComputerName'=$computer;
+                            'Manufacturer'=$CS.Manufacturer;
+                            'LastBootTime'=$OS.ConvertToDateTime($os.LastBootUpTime);
+                            'OSVersion'=$OS.Version
+                            }
+            $object = New-Object -TypeName PSObject -Property $properties
+
+            Write-Output $object
+
+        }    
+
+    }
+
+    END {
+
+    }
+
+}
