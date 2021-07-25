f56c0606e4af075671496d1928ceafbefd5b72f1
emusteric <ben.hollamby@outlook.com>
2021-07-25 17:50:39 +1200

    Couple of new labs

---
+++ Get-RemoteSMBShare.ps1
@@ -0,0 +1,92 @@
+﻿<#
+Lab One
+create a function called Get-RemoteSMBShare, it should accept one or more computer names
+either on the parameter or from the pipeline and then retrieve a list of current shared folders from each computer. 
+The output must include each computers name, the sharename, the description and the path to the share
+You'll be using invoke-command (Get-WmiObject -Class Win32_Share as get-smb now supports remoting)
+
+fancy it with:
+Computer name should be mandatory
+Computer name should accept from pipeline
+add alias of hostname for computername
+ensure that at least 1 and no more that five computers are allowed each run
+
+fancy it further with:
+error handling with an output file and a switch to enable/disable, and some warnings with verbose
+
+
+#>
+
+function Get-RemoteSMBShare {
+
+    [cmdletbinding()]
+
+    param (
+
+        [Parameter(Mandatory = $True,
+                   ValueFromPipeline = $True)]
+
+        [string[]]$ComputerName,
+
+        [string]$ErrorLogs = "C:\Temp\SMBErrors.txt",
+        [switch]$LogErrors
+
+    )
+
+    BEGIN {
+
+        Write-Verbose "Querying Computers"
+
+    }
+
+    PROCESS {
+
+        foreach ($Computer in $ComputerName) {
+
+            Try {
+
+                $Continue = $True
+
+                Invoke-Command -ComputerName $Computer -ScriptBlock {
+
+                    $Shares = Get-SmbShare
+
+                } -ErrorAction Stop
+
+            } Catch {
+
+                $Continue = $False
+
+                if ($LogErrors) {
+
+                    $Computer | Out-File $ErrorLogs -Append
+
+                    Write-Warning "Unable to connect to $Computer"
+
+                }
+
+              }
+
+            if ($Continue) {
+
+                $properties = @{'Computer'=$Computer;
+                                'ShareName'=$Shares.Name;
+                                'Description'=$Shares.Path;
+                                'Path'=$Shares.Description
+                                }
+
+                $objects = New-Object -TypeName PSObject -Property $properties
+
+                Write-Output $objects
+
+            }
+
+        }
+
+    }
+
+    END {
+
+    }
+
+}
---
+++ Set-ComputerState.ps1
@@ -0,0 +1,102 @@
+﻿<#
+Lab Two
+write a function called set-computerstate based on the Win32Shutdown method in Win32_OperatingSystem
+have it accept one or more computer names
+have it provide an -Action parameter which accepts on the values
+'LogOff', 'Restart', 'Shutdown', 'PowerOff' (0, 1, 2, 8 respectively)
+Provide a -Force switch (which is 4 (above) for force)
+Implement -whatif and -confirm with [CmdletBinding(SupportsShouldProcess=$True, ConfirmImpact='High')]
+
+Parameters not showing up! Comeback to this tomorrow!
+
+#>
+
+function Set-ComputerState {
+
+    [cmdletbinding(SupportShouldProcess = $True,
+                   ConfirmImpact='High')]
+
+    param (
+
+        [Parameter(Mandatory = $True,
+                   ValueFromPipeline = $True)]
+        [string[]]$ComputerName,
+
+        [ValidateNotNullorEmpty()] 
+        [ValidateSet(0, 1, 2, 8)]
+        [int]$Action,
+
+        [string]$ForceShutdown,
+        [switch]$Force
+        
+    )
+
+    BEGIN{
+
+    }
+
+    PROCESS {
+
+        foreach ($Computer in $ComputerName) {
+
+            Try {
+
+                if ($Force) {
+
+                    Write-Verbose "Forcing Shutdown"
+
+                    (Get-WmiObject -ComputerName $Computer -Class Win32_operatingsystem).Win32Shutdown(4)
+
+                } 
+                
+                elseif ($Action -eq 0) {
+
+                    Write-Verbose "Logging off user on $Computer"
+
+                    (Get-WmiObject -ComputerName $Computer -Class Win32_operatingsystem).Win32Shutdown(0)
+
+                }
+
+                elseif ($Action -eq 1) {
+
+                    Write-Verbose "Restarting $Computer"
+
+                    (Get-WmiObject -ComputerName $Computer -Class Win32_operatingsystem).Win32Shutdown(1)
+
+                }
+
+                elseif ($Action -eq 2) {
+
+                    Write-Verbose "Shutting down $Computer"
+
+                    (Get-WmiObject -ComputerName $Computer -Class Win32_operatingsystem).Win32Shutdown(2)
+                    
+                  
+                }
+
+                elseif ($Action -eq 8) {
+
+                    Write-Verbose "Powering off $Computer"
+
+                    (Get-WmiObject -ComputerName $Computer -Class Win32_operatingsystem).Win32Shutdown(8)
+
+                }
+
+                
+        }
+
+        Catch {
+
+            Write-Warning "Error!!!"
+
+            }
+
+    }
+
+}
+
+    END {
+
+    }
+
+}
