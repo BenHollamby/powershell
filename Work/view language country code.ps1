006a9d91097171fa4541ce271d3881833283041e
emusteric <ben.hollamby@outlook.com>
2021-08-02 16:47:28 +1200

    more work

---
+++ Work/view language country code.ps1
@@ -0,0 +1,7 @@
+﻿$users = Get-ADUser -Filter * | Where-Object {$_.Name -like "drone*"} | Select-Object -ExpandProperty SamAccountName
+
+foreach ($user in $users) {
+
+    Get-ADuser -Identity $user | Get-ADObject -Properties * | Select-Object sAMAccountName, c, co, countryCode, Country, preferredlanguage |ft
+    
+    }
