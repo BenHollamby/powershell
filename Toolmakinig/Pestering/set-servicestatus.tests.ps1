5078ee372690264719b3d55c45e790dc42bea5a7
emusteric <ben.hollamby@outlook.com>
2021-08-23 11:29:46 +1200

    pestering intro

---
+++ Toolmakinig/Pestering/set-servicestatus.tests.ps1
@@ -0,0 +1,28 @@
+﻿Describe "Set-ServiceStatus" {
+
+    It "starts BITS" {
+
+        Stop-Service BITS
+        $result = Set-ServiceStatus BITS
+        $result.status | Should Be 'Running'
+
+    }
+
+    It "starts BITS, skips FAKE" {
+
+        Stop-Service BITS
+        $result = Set-ServiceStatus BITS,FAKE
+        $result.status | Should Be 'Running'
+
+    }
+
+    It "starts 2 services" {
+
+        Stop-Service BITS
+        $result = Set-ServiceStatus BITS,TimeBrokerSvc
+        $result | Select -First 1 -ExpandProperty Status | Should Be 'Running'
+        $result | Select -Last 1 -ExpandProperty Status | Should Be 'Running'
+
+    }
+
+}
