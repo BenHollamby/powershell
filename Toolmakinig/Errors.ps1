2e211f35e9b0bb80109e5ec1283162a5ab047007
emusteric <ben.hollamby@outlook.com>
2021-08-22 16:08:16 +1200

    error fun

---
+++ Toolmakinig/Errors.ps1
@@ -0,0 +1,41 @@
+﻿function Get-Errors {
+
+    [cmdletbinding()]
+
+    param (
+
+    [switch]$LogErrors
+
+    )
+
+    $services = "bits","foo","Winrm"
+
+    foreach ($service in $services) {
+
+        Try {
+
+            Get-Service -Name $service -ErrorAction Stop
+
+        }
+
+        Catch {
+
+            $msg = "$Service does not exist as a service"
+
+            if ($LogErrors) {
+
+                "[$(Get-Date)] $msg" | Out-File C:\Temp\serviceerror.txt
+                
+            }
+
+            else {
+
+                Write-Warning $msg
+
+            }
+
+        }
+
+    }
+
+}
