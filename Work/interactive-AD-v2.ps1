cf7a61ffb5a1f639328ec3bbd7aa05a62895fea0
Ben Hollamby <62273111+emusteric@users.noreply.github.com>
2021-10-09 23:51:29 +1300

    functions for version 2 done, need another function to wrap around, test output, once happy, create user section

--- Work/interactive-AD-v2.ps1
+++ Work/interactive-AD-v2.ps1
@@ -1,4 +1,4 @@
-function New-MonarchName {
+function New-SwarmName {
 
     <#
     .SYNOPSIS
@@ -148,15 +148,384 @@ function New-MonarchName {
 
     END {
 
-        <# HASHED OUT, unhash for troubleshooting
-        $GivenName
-        $Surname
-        $DisplayName
-        $EmailAddress
-        $SAMAccountName
-        $UserPrincipalName
-        #>
+    }
+
+}
+
+function New-SwarmTitle {
+
+    <#
+    .SYNOPSIS
+    New-SwarmTitle will take a job title and capitalise it.
+    .DESCRIPTION
+    New-SwarmTitle takes an optional string of Title and capitalised the first letter of each string.
+    .PARAMETER Title
+    Optional
+    Accepts value from pipeline
+    Accepts value from pipeline by property name
+    .EXAMPLE
+    New-SwarmTitle -Title "team lead"
+    Team Lead
+    .EXAMPLE
+    "chief technology officer" | New-SwarmTitle
+    Chief Technology Officer
+    .EXAMPLE
+    Import-CSV C:\Temp\title.csv | New-SwarmTitle
+    #>  
+
+    [cmdletbinding()]
+
+    param (
+
+        [Parameter(ValueFromPipeline,
+                    ValueFromPipelineByPropertyName)]
+        [string]$Title
+
+    )
+
+    if ($Title) {
+
+        Write-Verbose "Capitalising Title"
+        $Title = (Get-Culture).TextInfo.ToTitleCase($Title)
 
     }
 
 }
+
+function New-SwarmBranch {
+
+    <#
+    .SYNOPSIS
+    New-SwarmBranch will take a branch and address and capitalise it.
+    .DESCRIPTION
+    New-SwarmBranch takes an optional string of Branch and or Address and capitalise the first letter of each string.
+    .PARAMETER Branch
+    Optional
+    Accepts value from pipeline
+    Accepts value from pipeline by property name
+    .PARAMETER Address
+    Optional
+    Accepts value from pipeline
+    Accepts value from pipeline by property name
+    .EXAMPLE
+    New-SwarmBranch -Branch "head office" -Address "666 electric blvd"
+    Head Office
+    666 Electric Blvd
+    .EXAMPLE
+    Import-CSV C:\Temp\branch.csv | New-SwarmBranch
+    Head Office
+    666 Electric Blvd
+    #>  
+
+    [cmdletbinding()]
+
+    param (
+
+        [Parameter(ValueFromPipeline,
+                    ValueFromPipelineByPropertyName
+                    )]
+        [string]$Branch,
+
+        [Parameter(ValueFromPipeline,
+                   ValueFromPipelineByPropertyName
+                   )]
+        [string]$Address
+
+    )
+
+    if ($Branch) {
+
+        Write-Verbose "Capitalising Branch"
+        $Branch = (Get-Culture).TextInfo.ToTitleCase($Branch)
+
+    }
+
+    if($Address) {
+
+        Write-Verbose "Capitalising Address"
+        $Address = (Get-Culture).TextInfo.ToTitleCase($Address)
+
+    }
+
+    $Branch
+    $Address
+
+}
+
+function New-SwarmMobile {
+
+    <#
+    .SYNOPSIS
+    New-SwarmMobile will take a Mobile split it to a more readable format.
+    .DESCRIPTION
+    New-SwarmMobile takes an optional string of a Mobile number and splits it into a 3-3-4 format
+    to make it more readable for humans.
+    .PARAMETER Mobile
+    Optional
+    Accepts value from pipeline
+    Accepts value from pipeline by property name
+    .EXAMPLE
+    New-SwarmMobile -Mobile 022222222 
+    022 222 2222
+    .EXAMPLE
+    022222222 | New-SwarmMobile  
+    022 222 2222
+    .EXAMPLE
+    Import-CSV C:\Temp\Mobile.csv | New-SwarmMobile
+    022 222 2222
+    #>  
+
+    [cmdletbinding()]
+
+    param (
+
+        [Parameter(ValueFromPipeline,
+                    ValueFromPipelineByPropertyName
+                    )]
+        [string]$Mobile
+
+       
+    )
+
+    if ($Mobile) {
+
+
+
+        if ($Mobile[0] -eq '0') {
+            
+            Write-Verbose "Spliting Mobile to friendly number"
+            $First3 = $Mobile.Substring(0,3)
+            $Second3 = $Mobile.Substring(3,3)
+            $LastDigits = $Mobile.Substring(6)
+            $Mobile = "$First3 $Second3 $LastDigits"
+        }
+        
+        
+        elseif ($Mobile[0] -ne '0') {
+
+            Write-Verbose "Mobile number from pipeline, appending 0 to the front"
+            $Mobile = "0$Mobile"
+            $First3 = $Mobile.Substring(0,3)
+            $Second3 = $Mobile.Substring(3,3)
+            $LastDigits = $Mobile.Substring(6)
+            $Mobile = "$First3 $Second3 $LastDigits"
+
+        }
+        
+    }
+
+    
+
+}
+
+function New-SwarmManager {
+
+    <#
+    .SYNOPSIS
+    New-SwarmManager will take a manager and grab the AD Object.
+    .DESCRIPTION
+    New-SwarmManager takes an optional string of a manager and if the user exists in Active Directory
+    puts the ad user into a variable.
+    .PARAMETER Manager
+    Optional
+    Accepts value from pipeline
+    Accepts value from pipeline by property name
+    .EXAMPLE
+    New-SwarmManager -Manager "drone one"
+    CN=Drone One,OU=Swarm Users,OU=Swarm,DC=swarm,DC=com
+    .EXAMPLE
+    "drone one" | New-SwarmManager  
+    CN=Drone One,OU=Swarm Users,OU=Swarm,DC=swarm,DC=com
+    .EXAMPLE
+    Import-CSV C:\Temp\Manager.csv | New-SwarmManager
+    CN=Drone One,OU=Swarm Users,OU=Swarm,DC=swarm,DC=com
+    #>  
+
+    [cmdletbinding()]
+
+    param (
+
+        [Parameter(ValueFromPipeline,
+                    ValueFromPipelineByPropertyName
+                    )]
+        [string]$Manager
+
+       
+    )
+
+    if ($Manager) {
+
+        Write-Verbose "Confirming if $Manager exists in domain"
+        if (Get-ADUser -Filter * | Where-Object {$_.Name -eq "$Manager"}) {
+
+            $Manager = Get-ADUser -Filter * | Where-Object {$_.Name -eq "$Manager"}
+
+        }
+
+    }
+
+}
+
+function New-SwarmPermissions {
+
+    <#
+    .SYNOPSIS
+    New-SwarmPermissions will get a users group memberships and assign them to a variable.
+    .DESCRIPTION
+    New-SwarmPermissions takes an optional string of a user, searches for that user in 
+    Active Directory, grabs all groups memberships and assigns them to a variable.
+    .PARAMETER Mobile
+    Optional
+    Accepts value from pipeline
+    Accepts value from pipeline by property name
+    .EXAMPLE
+    New-SwarmPermissions -Permissions "drone one" 
+    CN=Hive02,OU=Swarm Groups,OU=Swarm,DC=swarm,DC=com
+    CN=Hive01,OU=Swarm Groups,OU=Swarm,DC=swarm,DC=com
+    .EXAMPLE
+    'drone one' | New-SwarmPermissions  
+    CN=Hive02,OU=Swarm Groups,OU=Swarm,DC=swarm,DC=com
+    CN=Hive01,OU=Swarm Groups,OU=Swarm,DC=swarm,DC=com
+    .EXAMPLE
+    Import-CSV C:\Temp\copyuser.csv | New-SwarmPermissions
+    CN=Hive02,OU=Swarm Groups,OU=Swarm,DC=swarm,DC=com
+    CN=Hive01,OU=Swarm Groups,OU=Swarm,DC=swarm,DC=com
+    #>  
+
+    [cmdletbinding()]
+
+    param (
+
+        [Parameter(ValueFromPipeline,
+                    ValueFromPipelineByPropertyName
+                    )]
+        [string]$Permissions
+
+       
+    )
+
+    if ($Permissions) {
+
+        if (Get-ADUser -Filter * | Where-Object {$_.Name -eq "$Permissions"}) {
+
+            $GroupMemberShips = Get-ADUser -Filter * | Where-Object {$_.Name -eq "$Permissions"}
+            
+            $Groups = (Get-ADUser $GroupMemberShips -Properties MemberOf).MemberOf
+
+            $Groups
+
+        }
+        
+    }
+
+}
+
+function Get-RandomString {
+
+    <#
+    .SYNOPSIS
+    Get-RandomString will generate a random string of normal and special characters
+    .DESCRIPTION
+    Get-RandomString takes a mandatory input of Number which specifies how many characters
+    to generate. With just the Number parameter it will produce a string with normal
+    and special characters.
+    With the Specials parameter, users can specify how many special characters are
+    required in the string
+    .PARAMETER Number
+    Mandatory
+    Accepts value from pipeline
+    Determines how many characters will be in the string
+    .PARAMETER Specials
+    Optional
+    Determines how many special characters required in the string.
+    .EXAMPLE
+    Get-RandomString -Number 10 -Specials 9
+    <&:)_!@j:?
+    #>
+    
+        [cmdletbinding()]
+    
+        param (
+    
+            [Parameter(Mandatory,
+                       ValueFromPipeline)]
+            [int]$Number,
+    
+            [int]$Specials
+    
+        )
+    
+        $Count = 0
+    
+        $SCharacters = ""
+    
+        while ($Count -ne $Number) {
+    
+            foreach($l in $Number) {
+    
+                $Count += 1
+    
+                $Characters = 33..126
+                $Random = Get-Random $Characters
+                $string = [char]$Random
+    
+                $SCharacters += $string
+    
+            }
+    
+        }
+    
+        if($Specials) {
+    
+             DO {
+    
+                    $Count = 0
+    
+                    $SCharacters = ""
+    
+                    while ($Count -ne $Number) {
+    
+                        foreach($l in $Number) {
+    
+                            $Count += 1
+    
+                            $Characters = 33..126
+                            $Random = Get-Random $Characters
+                            $string = [char]$Random
+    
+                            $SCharacters += $string
+    
+                        }
+    
+                    }
+    
+                    $arrays = $SCharacters.ToCharArray()
+    
+                    $normal = ''
+                    $special = ''
+    
+                    foreach ($array in $arrays) {
+    
+                        if ($array -match '[^a-zA-Z0-9]') {
+    
+                            $special += $array
+    
+                        } else {
+    
+                            $normal += $array
+    
+                        }
+    
+                    }
+    
+                } Until ($special.Length -ge $Specials)
+    
+                $SCharacters
+    
+            } else {
+    
+                $SCharacters
+    
+            }
+    
+    } 
