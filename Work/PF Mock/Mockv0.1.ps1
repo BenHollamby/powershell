57834e54db2713c4fbd24d1689babe0a06c0a41c
Ben Hollamby <62273111+emusteric@users.noreply.github.com>
2021-10-12 20:46:41 +1300

    pre-reqs for first trial

---
+++ Work/PF Mock/Mockv0.1.ps1
@@ -0,0 +1,24 @@
+<#
+1.
+OU = PFO'''' for NZ staff
+OU = PFO''''''''''' for contracters 
+OU = PFO''' for aussies
+
+2.
+Permissions from x
+
+3.
+Normal User = first four letters of lastname and first letter of firstname, all one word
+Contractor = first four letters of lastname, underscore, first letter of firstname
+
+4.
+User must change password at next login
+
+5
+office
+department
+Title
+company
+manager
+Distribution groups that are listed.
+#>
