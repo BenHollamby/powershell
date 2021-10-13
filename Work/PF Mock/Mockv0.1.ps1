583c2a321c2ac7b0b4181806a09f7e661d2e2840
Ben Hollamby <62273111+emusteric@users.noreply.github.com>
2021-10-13 17:05:32 +1300

    information needed gathering

--- Work/PF Mock/Mockv0.1.ps1
+++ Work/PF Mock/Mockv0.1.ps1
@@ -14,11 +14,33 @@ Contractor = first four letters of lastname, underscore, first letter of firstna
 4.
 User must change password at next login
 
-5
+5.
 office
 department
 Title
-company
+company      same as description
+Description same as company
 manager
+email
+webpage
+
+6.
 Distribution groups that are listed.
+
+7.
+Proxy addresses to include
+SMTP:Firstname.Lastname@swarm.com
+smtp:username@swarm.com
+smtp:Firstname.Lastname@swarm.co.nz
+smtp:username@swarm.co.nz
+
+8.
+AD attributes
+c NZ|AU
+co New Zealand | Australia
+countryCode 554 | 036 (nz - aus respectively)
+
+9. 
+license with M365
+
 #>
