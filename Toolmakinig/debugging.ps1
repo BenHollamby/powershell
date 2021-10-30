53875f37f70aff0cf63fc0be513790ec744c2a01
Ben Hollamby <62273111+emusteric@users.noreply.github.com>
2021-10-31 11:41:33 +1300

    test debugging and pestering

---
+++ Toolmakinig/debugging.ps1
@@ -0,0 +1,57 @@
+function Test-Debuggin {
+
+    [CmdletBinding()]
+
+    param (
+
+        [Parameter(ValueFromPipeline,
+                    ValueFromPipelineByPropertyName
+                    )]
+        [int]$Number
+
+    )
+
+    BEGIN {
+
+    }
+
+    PROCESS {
+
+        Write-Debug "Number is $Number"
+        $R = $Number * $Number
+        Write-Debug "$Number x $Number = $R"
+
+    }
+
+    END {
+
+        $R
+
+    }
+
+}
+
+Describe "Test-Debuggin" {
+
+    It "times a number by itself" {
+
+        $Result = Test-Debuggin -Number 3
+        $result | Should Be 9
+
+    }
+
+    It "times a number by itself" {
+
+        $Result = Test-Debuggin -Number 6
+        $result | Should Be 36
+
+    }
+
+    It "times a number by itself" {
+
+        $Result = Test-Debuggin -Number 0
+        $result | Should Be 0
+
+    }
+
+}
