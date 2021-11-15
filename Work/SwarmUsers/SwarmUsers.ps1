3839246f8ec24ff0fda5e6727e62394c5f94e44e
Ben Hollamby <62273111+emusteric@users.noreply.github.com>
2021-11-16 12:58:14 +1300

    trying out this test driven development...

--- Work/SwarmUsers/SwarmUsers.ps1
+++ Work/SwarmUsers/SwarmUsers.ps1
@@ -1,10 +1,71 @@
-function Test-Number {
+function Format-SwarmNamePermanent {
 
-    Param (
-    [int]$Number
+    [cmdletbinding()]
+
+    param (
+
+        [Parameter(Mandatory,
+                    ValueFromPipeline,
+                    ValueFromPipelineByPropertyName
+                    )]
+        [string]$Name
+
     )
 
-    $Number * $Number
-    
+    foreach ($i in $Name) {
 
+        $GivenName            = (Get-Culture).TextInfo.ToTitleCase($i.Split()[0]) #Sets variable of full name to titlecase.
+        $Surname              = (Get-Culture).TextInfo.ToTitleCase($i.Split()[1]) #Sets variable of last name to titlecase.
+        $FirstName            = (Get-Culture).TextInfo.ToTitleCase($i.Split()[0]) #Sets variable of first name to titlecase. Doubled up so the New-ADUser parameters are clearer.
+        $LastName             = (Get-Culture).TextInfo.ToTitleCase($i.Split()[1]) #Sets variable of last name to titlecase. Doubled up so the New-ADUser parameters are clearer.
+        $DisplayNamePreSuffix = (Get-Culture).TextInfo.ToTitleCase($i)            #Sets variable of displayt name to titlecase.
+        $DisplayName          = "$DisplayNamePreSuffix$SuffixValue"
+        
+        if ($LastName.Length -eq 3) {                                             #if lastname is less than 3 do
+
+            $Length = $LastName.Length                                            #Gets length of last name  
+            $LastNameMod = $LastName.Substring(0,$Length)                         #Gets length number of characters 
+            $FirstNameMod = $FirstName.Substring(0,2)                             #Gets first two letters of the firstname
+            
+        } #end of lastname equals 3 block
+        
+        elseif ($lastname.Length -le 2) {                                         #if name less than or equal to two characters  
+        
+            $Length = $LastName.Length                                            #Gets length of last name  
+            $LastNameMod = $LastName.Substring(0,$Length)                         #Gets length number of characters
+            $FirstNameMod = $FirstName.Substring(0,3)                             #Gets first three letters of the firstname
+        
+        } #end of last name less than or equal to block
+        
+        else {
+        
+            $LastNameMod = $LastName.Substring(0,4)                               #grabs the first four characters of last name.   
+            $FirstNameMod = $firstname[0]                                         #grabs the first character of the first name.   
+            
+        } #end of catch all block
+
+        $UserNameMod          = "$LastNameMod$FirstNameMod$SuffixValue"           #joins last four characters of the last name and the first character of the first name in a variable
+        $UserName             = (Get-Culture).TextInfo.ToLower($UserNameMod)      #sets the username variable to lowercase
+        $EmailAddress         = "$FirstName.$LastName$SuffixValue@Swarm.com"      #creates the email address variable
+        $PrimarySMTP          = $EmailAddress                                     #Primary SMTP variable
+        $ProxyAddress1        = "$UserName@swarm.com"                             #Proxy address username .com             
+        $ProxyAddress2        = "$UserName@swarm.co.nz"                           #proxy address username .co.nz
+        $ProxyAddress3        = "$FirstName.$LastName$SuffixValue@swarm.co.nz"    #proxy address first name last name .co.nz
+
+    }
+
+    $GivenName 
+    $Surname            
+    $FirstName  
+    $LastName     
+    $DisplayNamePreSuffix
+    $DisplayName 
+    $UserNameMod 
+    $UserName       
+    $EmailAddress  
+    $PrimarySMTP   
+    $ProxyAddress1 
+    $ProxyAddress2 
+    $ProxyAddress3  
+    
 }
--- Work/SwarmUsers/test/SwarmUsers.Tests.ps1
+++ Work/SwarmUsers/test/SwarmUsers.Tests.ps1
@@ -1,16 +1,75 @@
 $ModuleManifestName = 'SwarmUsers.psd1'
 $ModuleManifestPath = "C:\Users\Kallor\devops\powershell\Work\SwarmUsers\$ModuleManifestName"
 
-Describe 'Module Manifest Tests' {
-    It 'Passes Test-ModuleManifest' {
+Describe 'Module Manifest Lowers' {
+    It 'Passes Lower-ModuleManifest' {
         Test-ModuleManifest -Path $ModuleManifestPath | Should -Be $true
 
     }
 }
 
-Describe 'Test-Number' {
+Describe 'Format-SwarmNamePermanent' {
 
-    It "times number by itself" {
-        Test-Number -Number 3 | Should -Be 9
+    BeforeAll {
+
+        $Lower = Format-SwarmNamePermanent -Name "iskaral pust"
+        $Upper = Format-SwarmNamePermanent -Name "ISKARAL PUST"
+        $Pipeline = 'iskaral pust' | Format-SwarmNamePermanent
+
     }
+
+    It "Spilts the first name and title case" {
+
+        $Lower[0] | Should -Be "Iskaral"
+        $Lower[1] | Should -Be "Pust"
+        $Lower[2] | Should -Be "Iskaral"
+        $Lower[3] | Should -Be "Pust"
+        $Lower[4] | Should -Be "Iskaral Pust"
+        $Lower[5] | Should -Be "Iskaral Pust"
+        $Lower[6] | Should -Be "PustI"
+        $Lower[7] | Should -Be "pusti"
+        $Lower[8] | Should -Be "Iskaral.Pust@Swarm.com"
+        $Lower[9] | Should -Be "Iskaral.Pust@Swarm.com"
+        $Lower[10] | Should -Be "pusti@swarm.com"
+        $Lower[11] | Should -Be "pusti@swarm.co.nz"
+        $Lower[12] | Should -Be "Iskaral.Pust@swarm.co.nz"
+
+    }
+
+    It "Spilts the first name, lowercases the name and title cases" {
+
+        $Upper[0] | Should -Be "Iskaral"
+        $Upper[1] | Should -Be "Pust"
+        $Upper[2] | Should -Be "Iskaral"
+        $Upper[3] | Should -Be "Pust"
+        $Upper[4] | Should -Be "Iskaral Pust"
+        $Upper[5] | Should -Be "Iskaral Pust"
+        $Upper[6] | Should -Be "PustI"
+        $Upper[7] | Should -Be "pusti"
+        $Upper[8] | Should -Be "Iskaral.Pust@Swarm.com"
+        $Upper[9] | Should -Be "Iskaral.Pust@Swarm.com"
+        $Upper[10] | Should -Be "pusti@swarm.com"
+        $Upper[11] | Should -Be "pusti@swarm.co.nz"
+        $Upper[12] | Should -Be "Iskaral.Pust@swarm.co.nz"
+
+    }
+
+    It "Spilts the first name, lowercases the name and title cases" {
+
+        $Pipeline[0] | Should -Be "Iskaral"
+        $Pipeline[1] | Should -Be "Pust"
+        $Pipeline[2] | Should -Be "Iskaral"
+        $Pipeline[3] | Should -Be "Pust"
+        $Pipeline[4] | Should -Be "Iskaral Pust"
+        $Pipeline[5] | Should -Be "Iskaral Pust"
+        $Pipeline[6] | Should -Be "PustI"
+        $Pipeline[7] | Should -Be "pusti"
+        $Pipeline[8] | Should -Be "Iskaral.Pust@Swarm.com"
+        $Pipeline[9] | Should -Be "Iskaral.Pust@Swarm.com"
+        $Pipeline[10] | Should -Be "pusti@swarm.com"
+        $Pipeline[11] | Should -Be "pusti@swarm.co.nz"
+        $Pipeline[12] | Should -Be "Iskaral.Pust@swarm.co.nz"
+
+    }
+
 }
