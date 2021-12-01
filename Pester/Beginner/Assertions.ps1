1570ac8dff55055c0cc5d692b845ed99e51f0cc3
Emusteric <ben.hollamby@outlook.com>
2021-12-01 19:50:06 +1300

    event logs to function

--- Pester/Beginner/Assertions.ps1
+++ Pester/Beginner/Assertions.ps1
@@ -1,3 +1,112 @@
+#BeNullOrEmpty
+
+$variable = 'somevalue'
+
+Describe 'BeNullOrEmpty' {
+
+    it 'tests variable is not empty' {
+        
+        $variable | Should -Not -BeNullOrEmpty
+    
+    }
+
+}
+
+#Be
+#the be operater tests for a simple thing like a string or a 
+#integer
+
+$string = 'somevalue'
+
+Describe 'Be' {
+
+    it 'tests string' {
+        
+        $string | Should -Be 'somevalue'
+    
+    }
+
+}
+
+$integer = 1
+
+Describe 'Be' {
+
+    it 'tests integer' {
+        
+        $integer | Should -Be 1
+    
+    }
+
+}
+
+#BeExactly
+#is the same as Be but tests for casesensitivity
+$string = 'SomeValue'
+
+Describe 'Be' {
+
+    it 'tests case sensitivity of a string' {
+        
+        $string | Should -BeExactly 'SomeValue'
+    
+    }
+
+}
+
+#BeLessThan 
+$Number = 2
+
+Describe 'BeLessThan' {
+
+    it 'tests Number is less than 4' {
+        
+        $Number | Should -BeLessThan 4
+    
+    }
+
+}
+
+#BeLessOrEqual
+$Number = 2
+
+Describe 'BeLessOrEqual' {
+    
+    it 'tests Number is less than or equal to 2 ' {
+
+    $Number | Should -BeLessOrEqual 2
+
+    }
+
+}
+
+#BeGreaterThan
+$Number = 2
+
+Describe 'BeGreaterThan' {
+
+    it 'tests Number is greater than 0' {
+        
+        $Number | Should -BeGreaterThan 0
+    
+    }
+
+}
+
+#BeGreaterOrEqual
+$Number = 2
+
+Describe 'BeGreaterOrEqual' {
+    
+    it 'tests Number is less than or equal to 2' {
+
+    $Number | Should -BeLessOrEqual 2
+
+    }
+
+}
+
+#BeLike
 $variable = 'word'
 
 Describe 'BeLike' {
@@ -10,6 +119,7 @@ Describe 'BeLike' {
 
 }
 
+#BeLikeExactly
 $variable = 'WORD'
 
 Describe 'BeLikeExactly' {
@@ -22,6 +132,7 @@ Describe 'BeLikeExactly' {
 
 }
 
+#Match
 $string = 'word'
 
 Describe 'Match' {
@@ -34,6 +145,7 @@ Describe 'Match' {
 
 }
 
+#MatchExactly
 $string = 'Word'
 
 Describe 'MatchExactly' {
@@ -46,6 +158,8 @@ Describe 'MatchExactly' {
 
 }
 
+
+#BeTrue
 $var = $true
 
 Describe 'BeTrue' {
@@ -58,6 +172,8 @@ Describe 'BeTrue' {
 
 }
 
+
+#BeFalse
 $var = $false
 
 Describe 'BeFalse' {
--- Work/EventLogs.ps1
+++ Work/EventLogs.ps1
@@ -1,127 +1,147 @@
-$Servers = Get-ADComputer -Filter {OperatingSystem -like '*Windows Server*'} | Where-Object {$_.Enabled -eq 'True'} | Select-Object -ExpandProperty Name
+function Get-SwarmEventLogs {
 
+    [cmdletbinding()]
 
-    $Yesterday = (Get-Date) - (New-TimeSpan -Day 1)
+    param()
 
-    $SystemErrorsTable        = @()
-    $SystemWarningsTable      = @()
-    $ApplicationErrorsTable   = @()
-    $ApplicationWarningsTable = @()
-    
-    
-    foreach ($Server in $Servers) {
-
-        $SystemErrors              = Get-WinEvent -LogName System -MaxEvents 500 -ComputerName $Server | Where-Object {$_.LevelDisplayName -eq "Error" -and $_.TimeCreated -ge $Yesterday}
-        $SystemWarnings            = Get-WinEvent -LogName System -MaxEvents 500 -ComputerName $Server | Where-Object {$_.LevelDisplayName -eq "Warning" -and $_.TimeCreated -ge $Yesterday}
-        $ApplicationErrors         = Get-WinEvent -LogName Application -MaxEvents 500 -ComputerName $Server | Where-Object {$_.LevelDisplayName -eq "Error" -and $_.TimeCreated -ge $Yesterday}
-        $ApplicationWarnings       = Get-WinEvent -LogName Application -MaxEvents 500 -ComputerName $Server | Where-Object {$_.LevelDisplayName -eq "Warning" -and $_.TimeCreated -ge $Yesterday}
-        
-
-        $SystemErrorCount          = $SystemErrors.count
-        $SystemWarningCount        = $SystemWarnings.count
-        $ApplicationErrorCount     = $ApplicationErrors.count
-        $ApplicationWarningCount   = $ApplicationWarnings.count
-        
-        
-        $SystemUniqueErrors        = $SystemErrors      | Select-Object -Unique
-        $SystemUniqueWarnings      = $SystemWarnings    | Select-Object -Unique
-        $ApplicationUniqueErrors   = $ApplicationErrors | Select-Object -Unique
-        $ApplicationUniqueWarnings = $SystemWarnings    | Select-Object -Unique
-        
-        foreach ($SystemUniqueError in $SystemUniqueErrors) {
-        
-            $SystemErrorArray      = [PSCustomObject] @{
-
-                Server             = $Server
-                Time               = $SystemUniqueError.TimeCreated
-                ID                 = $SystemUniqueError.ID
-                NumberofEvents     = $SystemErrorCount
-                Message            = $SystemUniqueError.Message
-                Type               = "Error"
-
-
-            }
-
-           $SystemErrorsTable      += $SystemErrorArray
-
-        }
-
-        foreach ($SystemUniqueWarning in $SystemUniqueWarnings) {
-
-            $SystemWarningArray = [PSCustomObject] @{
-
-                Server             = $Server
-                Time               = $SystemUniqueWarning.TimeCreated
-                ID                 = $SystemUniqueWarning.ID
-                NumberofEvents     = $SystemWarningCount
-                Message            = $SystemUniqueWarning.Message
-                Type               = "Warning"
-
-
-                }
-
-            $SystemWarningsTable   += $SystemWarningArray
-
-
-        }
-
-        foreach ($ApplicationUniqueError in $ApplicationUniqueErrors) {
-        
-            $ApplicationErrorArray = [PSCustomObject] @{
-
-                Server             = $Server
-                Time               = $ApplicationUniqueError.TimeCreated
-                ID                 = $ApplicationUniqueError.ID
-                NumberofEvents     = $ApplicationErrorCount
-                Message            = $ApplicationUniqueError.Message
-                Type               = "Error"
-
-
-            }
-
-           $ApplicationErrorsTable += $ApplicationErrorArray
-
-        }
-
-        foreach ($ApplicationUniqueWarning in $ApplicationUniqueWarnings) {
-
-            $ApplicationWarningArray = [PSCustomObject] @{
-
-                Server             = $Server
-                Time               = $ApplicationUniqueWarning.TimeCreated
-                ID                 = $ApplicationUniqueWarning.ID
-                NumberofEvents     = $ApplicationWarningCount
-                Message            = $ApplicationUniqueWarning.Message
-                Type               = "Warning"
-
-
-                }
-
-            $ApplicationWarningsTable += $ApplicationWarningArray
-
-
-        }
-
-      
+    BEGIN {
 
     }
 
-Write-Output ''
-Write-Output "SYSTEM ERRORS"
+    PROCESS {
 
-$SystemErrorsTable | Format-Table -AutoSize
+        $Servers = Get-ADComputer -Filter {OperatingSystem -like '*Windows Server*'} | Where-Object {$_.Enabled -eq 'True'} | Select-Object -ExpandProperty Name
 
-Write-Output ''
-Write-Output "SYSTEM WARNINGS"
 
-$SystemWarningsTable | Format-Table -AutoSize
+            $Yesterday = (Get-Date) - (New-TimeSpan -Day 1)
 
-Write-Output ''
-Write-Output "APPLICATION ERRORS"
+            $SystemErrorsTable        = @()
+            $SystemWarningsTable      = @()
+            $ApplicationErrorsTable   = @()
+            $ApplicationWarningsTable = @()
+            
+            
+            foreach ($Server in $Servers) {
 
-$ApplicationErrorsTable | Format-Table -AutoSize
+                $SystemErrors              = Get-WinEvent -LogName System -MaxEvents 500 -ComputerName $Server | Where-Object {$_.LevelDisplayName -eq "Error" -and $_.TimeCreated -ge $Yesterday}
+                $SystemWarnings            = Get-WinEvent -LogName System -MaxEvents 500 -ComputerName $Server | Where-Object {$_.LevelDisplayName -eq "Warning" -and $_.TimeCreated -ge $Yesterday}
+                $ApplicationErrors         = Get-WinEvent -LogName Application -MaxEvents 500 -ComputerName $Server | Where-Object {$_.LevelDisplayName -eq "Error" -and $_.TimeCreated -ge $Yesterday}
+                $ApplicationWarnings       = Get-WinEvent -LogName Application -MaxEvents 500 -ComputerName $Server | Where-Object {$_.LevelDisplayName -eq "Warning" -and $_.TimeCreated -ge $Yesterday}
+                
 
-Write-Output ''
-Write-Output "APPLICATION WARNINGS"
+                $SystemErrorCount          = $SystemErrors.count
+                $SystemWarningCount        = $SystemWarnings.count
+                $ApplicationErrorCount     = $ApplicationErrors.count
+                $ApplicationWarningCount   = $ApplicationWarnings.count
+                
+                
+                $SystemUniqueErrors        = $SystemErrors      | Select-Object -Unique
+                $SystemUniqueWarnings      = $SystemWarnings    | Select-Object -Unique
+                $ApplicationUniqueErrors   = $ApplicationErrors | Select-Object -Unique
+                $ApplicationUniqueWarnings = $SystemWarnings    | Select-Object -Unique
+                
+                foreach ($SystemUniqueError in $SystemUniqueErrors) {
+                
+                    $SystemErrorArray      = [PSCustomObject] @{
 
-$ApplicationWarningsTable | Format-Table -AutoSize
+                        Server             = $Server
+                        Time               = $SystemUniqueError.TimeCreated
+                        ID                 = $SystemUniqueError.ID
+                        NumberofEvents     = $SystemErrorCount
+                        Message            = $SystemUniqueError.Message
+                        Type               = "Error"
+
+
+                    }
+
+                $SystemErrorsTable      += $SystemErrorArray
+
+                }
+
+                foreach ($SystemUniqueWarning in $SystemUniqueWarnings) {
+
+                    $SystemWarningArray = [PSCustomObject] @{
+
+                        Server             = $Server
+                        Time               = $SystemUniqueWarning.TimeCreated
+                        ID                 = $SystemUniqueWarning.ID
+                        NumberofEvents     = $SystemWarningCount
+                        Message            = $SystemUniqueWarning.Message
+                        Type               = "Warning"
+
+
+                        }
+
+                    $SystemWarningsTable   += $SystemWarningArray
+
+
+                }
+
+                foreach ($ApplicationUniqueError in $ApplicationUniqueErrors) {
+                
+                    $ApplicationErrorArray = [PSCustomObject] @{
+
+                        Server             = $Server
+                        Time               = $ApplicationUniqueError.TimeCreated
+                        ID                 = $ApplicationUniqueError.ID
+                        NumberofEvents     = $ApplicationErrorCount
+                        Message            = $ApplicationUniqueError.Message
+                        Type               = "Error"
+
+
+                    }
+
+                $ApplicationErrorsTable += $ApplicationErrorArray
+
+                }
+
+                foreach ($ApplicationUniqueWarning in $ApplicationUniqueWarnings) {
+
+                    $ApplicationWarningArray = [PSCustomObject] @{
+
+                        Server             = $Server
+                        Time               = $ApplicationUniqueWarning.TimeCreated
+                        ID                 = $ApplicationUniqueWarning.ID
+                        NumberofEvents     = $ApplicationWarningCount
+                        Message            = $ApplicationUniqueWarning.Message
+                        Type               = "Warning"
+
+
+                        }
+
+                    $ApplicationWarningsTable += $ApplicationWarningArray
+
+
+                }
+
+            
+
+            }
+
+        Write-Output ''
+        Write-Output "SYSTEM ERRORS"
+
+        $SystemErrorsTable | Format-Table -AutoSize
+
+        Write-Output ''
+        Write-Output "SYSTEM WARNINGS"
+
+        $SystemWarningsTable | Format-Table -AutoSize
+
+        Write-Output ''
+        Write-Output "APPLICATION ERRORS"
+
+        $ApplicationErrorsTable | Format-Table -AutoSize
+
+        Write-Output ''
+        Write-Output "APPLICATION WARNINGS"
+
+        $ApplicationWarningsTable | Format-Table -AutoSize
+
+    }
+
+    END {
+
+    }
+
+}
