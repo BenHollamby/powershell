e77feb9ca11f5e55372c761a75e11c0e9bba9f82
Emusteric <ben.hollamby@outlook.com>
2021-12-09 19:32:33 +1300

    contractor names function and tests

--- Modules/PFOUtil/PFOUtil.psm1
+++ Modules/PFOUtil/PFOUtil.psm1
@@ -99,6 +99,107 @@ function Test-FormatName {
     $testobject
 
 }
+
+function Format-SwarmNameContractor {
+
+    [cmdletbinding()]
+
+    param(
+
+        [Parameter(Mandatory,
+                    ValueFromPipeline,
+                    ValueFromPipelineByPropertyName)]
+        [string]$Name
+
+    )
+
+    BEGIN {
+
+    }
+
+    PROCESS {
+        
+        $PrimaryDomain = "@swarm.com"
+        $SecondaryDomain = "@swarm.co.nz"
+
+        $Script:GivenName            = (Get-Culture).TextInfo.ToTitleCase($Name.Split()[0]) #Sets variable of full name to titlecase.
+        $Script:Surname              = (Get-Culture).TextInfo.ToTitleCase($Name.Split()[1]) #Sets variable of last name to titlecase.
+        $Script:FirstName            = (Get-Culture).TextInfo.ToTitleCase($Name.Split()[0]) #Sets variable of first name to titlecase. Doubled up so the New-ADUser parameters are clearer.
+        $Script:LastName             = (Get-Culture).TextInfo.ToTitleCase($Name.Split()[1]) #Sets variable of last name to titlecase. Doubled up so the New-ADUser parameters are clearer.
+        $Script:DisplayName          = "$FirstName $LastName"               #Sets Displayname with suffix to variable  
+        
+        if ($LastName.Length -eq 3) {                                             #if lastname is less than 3 do
+
+            $Length = $LastName.Length                                            #Gets length of last name  
+            $LastNameMod = $LastName.Substring(0,$Length)                         #Gets length number of characters 
+            $FirstNameMod = $FirstName.Substring(0,2)                             #Gets first two letters of the firstname
+            
+        } #end of lastname equals 3 block
+        
+        elseif ($lastname.Length -le 2) {                                         #if name less than or equal to two characters  
+        
+            $Length = $LastName.Length                                            #Gets length of last name  
+            $LastNameMod = $LastName.Substring(0,$Length)                         #Gets length number of characters
+            $FirstNameMod = $FirstName.Substring(0,3)                             #Gets first three letters of the firstname
+        
+        } #end of last name less than or equal to block
+        
+        else {
+        
+            $LastNameMod = $LastName.Substring(0,4)                               #grabs the first four characters of last name.   
+            $FirstNameMod = $firstname[0]                                         #grabs the first character of the first name.   
+            
+        } #end of catch all block
+
+        $ContractorMod        = "_"                                                   #underscore variable for contractor
+        $UserNameMod          = "$LastNameMod$ContractorMod$FirstNameMod" #joins last four characters of the last name and the first character of the first name in a variable
+        $Script:UserName             = (Get-Culture).TextInfo.ToLower($UserNameMod)          #sets the username variable to lowercase
+        $Script:EmailAddress         = "$FirstName.$LastName$PrimaryDomain"          #creates the email address variable
+        $Script:PrimarySMTP          = $EmailAddress                                         #Primary SMTP variable
+        $Script:ProxyAddress1        = "$UserName$PrimaryDomain"                                 #Proxy address username .com             
+        $Script:ProxyAddress2        = "$UserName$SecondaryDomain"                               #proxy address username .co.nz
+        $Script:ProxyAddress3        = "$FirstName.$LastName$SecondaryDomain"        #proxy address first name last name .co.nz
+
+
+    }
+
+    END {
+
+    }
+
+}
+
+function Test-FormatNameContractor {
+
+    [cmdletbinding()]
+
+    param (
+
+        [Parameter(Mandatory,
+                    ValueFromPipeline)]
+        $Object
+
+    )
+
+    $testobject = [PSCustomObject] @{
+
+        GivenName     = $GivenName
+        Surname       = $Surname
+        FirstName     = $FirstName
+        LastName      = $LastName 
+        DisplayName   = $DisplayName
+        UserName      = $UserName
+        EmailAddress  = $EmailAddress
+        PrimarySMTP   = $PrimarySMTP
+        ProxyAddress1 = $ProxyAddress1
+        ProxyAddress2 = $ProxyAddress2 
+        ProxyAddress3 = $ProxyAddress3
+
+    }
+
+    $testobject
+
+}
 # Export only the functions using PowerShell standard verb-noun naming.
 # Be sure to list each exported functions in the FunctionsToExport field of the module manifest file.
 # This improves performance of command discovery in PowerShell.
--- Modules/PFOUtil/test/PFOUtil.Tests.ps1
+++ Modules/PFOUtil/test/PFOUtil.Tests.ps1
@@ -18,6 +18,16 @@ Describe 'Format-SwarmName Parameters' -Tag 'Format-SwarmName' {
 
 }
 
+Describe 'Format-SwarmNameContractor Parameters' -Tag 'Format-SwarmNameContractor' {
+
+    It 'Tests for mandatory string Name parameter' {
+ 
+         Get-Command Format-SwarmNameContractor | Should -HaveParameter 'Name' -Type String -Mandatory
+ 
+    }
+ 
+ }
+
 Describe 'Tests output of Format-SwarmName' -Tag 'Format-SwarmName' {
 
     it 'Tests given name' {
@@ -34,6 +44,28 @@ Describe 'Tests output of Format-SwarmName' -Tag 'Format-SwarmName' {
         $Objects.PrimarySMTP   | Should -BeExactly 'Anomander.Rake@swarm.com'
         $Objects.ProxyAddress1 | Should -BeExactly 'rakea@swarm.com'
         $Objects.ProxyAddress2 | Should -BeExactly 'rakea@swarm.co.nz'
+        $Objects.ProxyAddress3 | Should -BeExactly 'Anomander.Rake@swarm.co.nz'
+
+    }
+
+}
+
+Describe 'Tests output of Contractor Name function' -Tag 'Format-SwarmNameContractor' {
+
+    it 'Tests given name' {
+
+        $Objects = Format-SwarmNameContractor -Name 'anomander rake' | Test-FormatNameContractor
+
+        $Objects.GivenName     | Should -BeExactly 'Anomander'
+        $Objects.Surname       | Should -BeExactly 'Rake'
+        $Objects.FirstName     | Should -BeExactly 'Anomander'
+        $Objects.LastName      | Should -BeExactly 'Rake'
+        $Objects.DisplayName   | Should -BeExactly 'Anomander Rake'
+        $Objects.UserName      | Should -BeExactly 'rake_a'
+        $Objects.EmailAddress  | Should -BeExactly 'Anomander.Rake@swarm.com'
+        $Objects.PrimarySMTP   | Should -BeExactly 'Anomander.Rake@swarm.com'
+        $Objects.ProxyAddress1 | Should -BeExactly 'rake_a@swarm.com'
+        $Objects.ProxyAddress2 | Should -BeExactly 'rake_a@swarm.co.nz'
         $Objects.ProxyAddress3 | Should -BeExactly 'Anomander.Rake@swarm.co.nz'
 
     }
