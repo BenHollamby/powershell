64e31e1c067dde91b9bd05ccfbb41e1e74a16ffe
Emusteric <ben.hollamby@outlook.com>
2021-12-27 17:38:50 +1300

    https pull server complete, unable to figure out how to add to http as well

--- DSC/PullServer/HTTPSPullServer.ps1
+++ DSC/PullServer/HTTPSPullServer.ps1
@@ -3,7 +3,7 @@ Configuration HTTPSPullServer {
     #xPSDesiredStateConfiguration module needs to be installed
     Import-DscResource -ModuleName xPSDesiredStateConfiguration
 
-    node 2022APP01 {
+    node 2022DSC01 {
 
         WindowsFeature DSCServiceFeature {
 
@@ -22,9 +22,9 @@ Configuration HTTPSPullServer {
         xDSCWebService PSDSCPullServer {
 
             Ensure                   = "Present"
-            EndpointName             = "SecurePSDSCPullServer"
+            EndpointName             = "PSDSCPullServer"
             Port                     = 10443
-            PhysicalPath             = "$env:SystemDrive\inetpub\wwwroot\SecurePSDSCPullServer"
+            PhysicalPath             = "$env:SystemDrive\inetpub\wwwroot\SSLPSDSCPullServer"
             CertificateThumbPrint    = "8bf1fc91121b4fdf4f184b31bdb4e4d5ad43ae12"
             ModulePath               = "$env:ProgramFiles\WindowsPowerShell\DscService\Modules"
             ConfigurationPath        = "$env:ProgramFiles\WindowsPowerShell\DscService\Configuration"
@@ -38,9 +38,9 @@ Configuration HTTPSPullServer {
         xDscWebService PSDSCComplianceServer {
 
             Ensure                   = "Present"
-            EndpointName             = "SecurePSDSCComplianceServer"
-            Port                     = 9443
-            PhysicalPath             = "$env:SystemDrive\inetpub\wwwroot\SecurePSDSCComplianceServer"
+            EndpointName             = "PSDSCComplianceServer"
+            Port                     = 11443
+            PhysicalPath             = "$env:SystemDrive\inetpub\wwwroot\PSDSCComplianceServer"
             CertificateThumbPrint    = "8bf1fc91121b4fdf4f184b31bdb4e4d5ad43ae12"
             State                    = "Started"
             DependsOn                = "[WindowsFeature]DSCServiceFeature","[xDSCWebService]PSDSCPullServer"
@@ -58,4 +58,4 @@ Configuration HTTPSPullServer {
 HTTPSPullServer -OutputPath C:\DSC\HTTPS
 
 #apply config
-Start-DscConfiguration -Path C:\DSC\HTTPS -ComputerName 2022APP01 -Verbose -Wait -Force
+Start-DscConfiguration -Path C:\DSC\HTTPS -ComputerName 2022DSC01 -Verbose -Wait -Force
